a19a3d2267251ac21ed9562daab63e4f9559d6a0
Gjert Ingar Gjersund <gjert@gjersund.com>
2019-09-04 00:56:55 +0200

    Fix config

--- chat/consumers.py
+++ chat/consumers.py
@@ -12,8 +12,10 @@ class ChatConsumer(WebsocketConsumer):
             self.room_group_name,
             self.channel_name
         )
-
         self.accept()
+        self.send(text_data=json.dumps({
+            'message': "Accepted to the chat channel!"
+        }))
 
     def disconnect(self, close_code):
         # Leave room group
@@ -26,7 +28,10 @@ class ChatConsumer(WebsocketConsumer):
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
-
+        # Echo back
+        self.send(text_data=json.dumps({
+            'message': message
+        }))
         # Send message to room group (from the user)
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
